b4203440d17b822b41a4b16ed878cac41fb885eb
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2023-03-28 00:07:45 +0000

    commit

--- gera_dados.py
+++ gera_dados.py
@@ -34,54 +34,48 @@ num_alunos_bom_em_tudo = num_alunos_ruim_em_tudo = num_alunos // 10
 num_alunos_restantes = num_alunos - num_alunos_bom_em_tudo - num_alunos_ruim_em_tudo
 num_alunos_exatas = num_alunos_humanas = num_alunos_restantes // 2
 
-# Gera notas para cada tipo de aluno
-notas_matematica = np.concatenate([
-    gerar_notas(num_alunos_exatas, 'exatas'),
-    gerar_notas(num_alunos_humanas, 'humanas'),
-    gerar_notas(num_alunos_bom_em_tudo, 'bom_em_tudo'),
-    gerar_notas(num_alunos_ruim_em_tudo, 'ruim_em_tudo')])
-
-notas_linguagens = np.concatenate([
-    gerar_notas(num_alunos_humanas, 'humanas'),
-    gerar_notas(num_alunos_exatas, 'exatas'),
-    gerar_notas(num_alunos_bom_em_tudo, 'bom_em_tudo'),
-    gerar_notas(num_alunos_ruim_em_tudo, 'ruim_em_tudo')])
-
-notas_ciencias_natureza = np.concatenate([
-    gerar_notas(num_alunos_exatas, 'exatas'),
-    gerar_notas(num_alunos_humanas, 'humanas'),
-    gerar_notas(num_alunos_bom_em_tudo, 'bom_em_tudo'),
-    gerar_notas(num_alunos_ruim_em_tudo, 'ruim_em_tudo')])
-
-notas_ciencias_humanas = np.concatenate([
-    gerar_notas(num_alunos_humanas, 'humanas'),
-    gerar_notas(num_alunos_exatas, 'exatas'),
-    gerar_notas(num_alunos_bom_em_tudo, 'bom_em_tudo'),
-    gerar_notas(num_alunos_ruim_em_tudo, 'ruim_em_tudo')])
-
-
-notas_redacao = np.concatenate([
-    gerar_notas(num_alunos_humanas, 'humanas'),
-    gerar_notas(num_alunos_exatas, 'exatas'),
-    gerar_notas(num_alunos_bom_em_tudo, 'bom_em_tudo'),
-    gerar_notas(num_alunos_ruim_em_tudo, 'ruim_em_tudo')])
+# Gera notas para cada matéria e tipo de aluno
+notas_matematica = gerar_notas(num_alunos, 'exatas')
+notas_fisica = gerar_notas(num_alunos, 'exatas')
+notas_biologia = gerar_notas(num_alunos, 'humanas')
+notas_quimica = gerar_notas(num_alunos, 'exatas')
+notas_lingua_portuguesa = gerar_notas(num_alunos, 'humanas')
+notas_lingua_inglesa = gerar_notas(num_alunos, 'humanas')
+notas_artes = gerar_notas(num_alunos, 'humanas')
+notas_filosofia = gerar_notas(num_alunos, 'humanas')
+notas_redacao = gerar_notas(num_alunos, 'humanas')
+notas_historia = gerar_notas(num_alunos, 'humanas')
+notas_geografia = gerar_notas(num_alunos, 'humanas')
 
 # Embaralha os dados
 embaralhar_indices = np.random.permutation(num_alunos)
+# Embaralha os dados
 notas_matematica = notas_matematica[embaralhar_indices]
-notas_linguagens = notas_linguagens[embaralhar_indices]
-notas_ciencias_natureza = notas_ciencias_natureza[embaralhar_indices]
-notas_ciencias_humanas = notas_ciencias_humanas[embaralhar_indices]
+notas_fisica = notas_fisica[embaralhar_indices]
+notas_biologia = notas_biologia[embaralhar_indices]
+notas_quimica = notas_quimica[embaralhar_indices]
+notas_lingua_portuguesa = notas_lingua_portuguesa[embaralhar_indices]
+notas_lingua_inglesa = notas_lingua_inglesa[embaralhar_indices]
+notas_artes = notas_artes[embaralhar_indices]
+notas_filosofia = notas_filosofia[embaralhar_indices]
 notas_redacao = notas_redacao[embaralhar_indices]
+notas_historia = notas_historia[embaralhar_indices]
+notas_geografia = notas_geografia[embaralhar_indices]
 
 # Cria um DataFrame com os dados gerados
 dados = pd.DataFrame({'ID': ids,
                       'Matemática': notas_matematica,
-                      'Linguagens': notas_linguagens,
-                      'Ciências da Natureza': notas_ciencias_natureza,
-                      'Ciências Humanas': notas_ciencias_humanas,
-                      'Redação': notas_redacao})
+                      'Física': notas_fisica,
+                      'Biologia': notas_biologia,
+                      'Química': notas_quimica,
+                      'Língua Portuguesa': notas_lingua_portuguesa,
+                      'Língua Inglesa': notas_lingua_inglesa,
+                      'Artes': notas_artes,
+                      'Filosofia': notas_filosofia,
+                      'Redação': notas_redacao,
+                      'História': notas_historia,
+                      'Geografia': notas_geografia})
 
 # Salva a base de dados em um arquivo CSV
-dados.to_csv('dados_alunos.csv', index=False)
+dados.to_csv('dados_alunos_materias.csv', index=False)
 print(dados)
